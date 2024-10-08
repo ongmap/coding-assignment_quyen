dfc1b5578e4eb98e8393f2d5f9539cef29e852ea
idiotman <quyentranqs@gmail.com>
2024-10-08 15:39:07 +0300

    Add Bootstrap lib

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { User } from "../../types/User";
 import styles from './UserCard.module.scss';
+import { Card } from "react-bootstrap";
 
 interface UserCardProps {
   user: User;
 }
 
 const UserCard: React.FC<UserCardProps> = ({ user }) => (
-  <div className={styles.card}>
-    <h3>{user.name}</h3>
-    <p>Email: {user.email}</p>
-    <p>Phone: {user.phone}</p>
-    <p>Website: <a href={`http://${user.website}`}>{user.website}</a></p>
-    <p>Address: {user.address.street}, {user.address.city}</p>
-  </div>
+	<Card className={styles.card}>
+		<Card.Body>
+			<Card.Title>{user.name}</Card.Title>
+			<Card.Text>
+					<strong>Username:</strong> {user.username}<br />
+					<strong>Email:</strong> {user.email}<br />
+					<strong>Address:</strong> {`${user.address.street}, ${user.address.suite}, ${user.address.zipcode}, ${user.address.city}`}<br />
+					<strong>Phone:</strong> {user.phone}<br />
+					<strong>Website:</strong> <a href={`http://${user.website}`} target="_blank" rel="noopener noreferrer">{user.website}</a><br />
+					<div className={styles.companyInfo}>
+						<strong>Company:</strong> {user.company.name}<br />
+						<strong>Catchphrase:</strong> {user.company.catchPhrase}<br />
+						<strong>Business:</strong> {user.company.bs}<br />
+					</div>
+			</Card.Text>
+		</Card.Body>
+	</Card>
 );
 
 export default UserCard;
