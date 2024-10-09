542f07cbb339e1d357ee5d9103a3fbc1c996d544
idiotman <quyentranqs@gmail.com>
2024-10-09 12:55:29 +0300

    Fix Card's styles

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -7,24 +7,30 @@ interface UserCardProps {
   user: User;
 }
 
-const UserCard: React.FC<UserCardProps> = ({ user }) => (
-	<Card className={styles.card}>
-		<Card.Body>
-			<Card.Title>{user.name}</Card.Title>
-			<Card.Text>
-					<strong>Username:</strong> {user.username}<br />
-					<strong>Email:</strong> {user.email}<br />
-					<strong>Address:</strong> {`${user.address.street}, ${user.address.suite}, ${user.address.zipcode}, ${user.address.city}`}<br />
-					<strong>Phone:</strong> {user.phone}<br />
-					<strong>Website:</strong> <a href={`http://${user.website}`} target="_blank" rel="noopener noreferrer">{user.website}</a><br />
-					<div className={styles.companyInfo}>
-						<strong>Company:</strong> {user.company.name}<br />
-						<strong>Catchphrase:</strong> {user.company.catchPhrase}<br />
-						<strong>Business:</strong> {user.company.bs}<br />
-					</div>
-			</Card.Text>
-		</Card.Body>
-	</Card>
-);
+const UserCard: React.FC<UserCardProps> = ({ user }) => {
+	const { street, suite, city, zipcode } = user.address;
+
+	return (
+		<Card className={styles.userCard}>
+			<Card.Body>
+				<Card.Title className={styles.cardTitle}>{user.name}</Card.Title>				
+				<Card.Text className={styles.cardText}>
+					<strong>Email:</strong> {user.email}
+				</Card.Text>
+				<Card.Text className={styles.cardText}>
+					<strong>Phone:</strong> {user.phone}
+				</Card.Text>
+				<Card.Text className={styles.cardText}>
+					<strong>Website:</strong> <a href={`http://${user.website}`} target="_blank" rel="noopener noreferrer">{user.website}</a>
+				</Card.Text>
+				
+				{/* Address Information */}
+				<Card.Text>
+					<strong>Address:</strong> {`${street}${suite ? `, ${suite}` : ''}, ${city}, ${zipcode}`}
+				</Card.Text>
+			</Card.Body>
+		</Card>
+	);
+}
 
 export default UserCard;
